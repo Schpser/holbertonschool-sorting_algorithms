0a0361df94dc972101bb8dccd8e5515208aab9f0
Schpser <schps.games@gmail.com>
2025-08-08 17:02:15 +0200

    Fix comment + add 1-O

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,8 +1,7 @@
 #include "sort.h"
 
 /**
- * insertion_sort_list - Sorts a doubly linked list of integers in ascending
- *                       order using the Insertion sort algorithm
+ * insertion_sort_list - Sorts a doubly linked list
  * @list: Double pointer to the head of the linked list
  */
 
